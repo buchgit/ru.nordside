68df06e9f684f6e18bde88784b2ac39cc4831022
buchgit <buchgit@gmail.com>
2021-04-01 23:40:04 +0300

    data sourse

---
+++ out/artifacts/ru_nordside_war_exploded/WEB-INF/jsp/index.jsp
@@ -0,0 +1,11 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Nordside</title>
+    <h2>Hello World!</h2>
+</head>
+<body>
+
+</body>
+</html>
--- src/main/java/nordside/web/RootController.java
+++ src/main/java/nordside/web/RootController.java
@@ -5,14 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/user/*")
 public class RootController {
 
     static final String REST_URL = "/";
 
-    @GetMapping("/")
-    public void getById(){
-
+    @GetMapping("/user/1")
+    public String root1(){
+        return "index";
+    }
+    @GetMapping("/user/2")
+    public String root2(){
+        return "index";
     }
 
 }
